a526f313e944843264c64a2434a77f5cdf958e4e
Eli Fatsi <eli.fatsi@viget.com>
2015-05-12 11:03:24 -0600

    Can fetch and display data for a Core

--- app/models/core.rb
+++ app/models/core.rb
@@ -1,4 +1,13 @@
 class Core < ActiveRecord::Base
   has_many :means
   has_many :variances
+
+  def fetch_data
+    means     << Mean.new(value: spark_core.variable("mean"))
+    variances << Variance.new(value: spark_core.variable("variance"))
+  end
+
+  def spark_core
+    @spark_core ||= RubySpark::Core.new(spark_id)
+  end
 end

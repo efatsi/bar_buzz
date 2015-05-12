6ee32d0853dea7ef2fc7c5ff6d9ed6850464ef3c
Eli Fatsi <eli.fatsi@viget.com>
2015-05-12 11:08:44 -0600

    Added rake task for collecting data

--- app/models/core.rb
+++ app/models/core.rb
@@ -2,10 +2,18 @@ class Core < ActiveRecord::Base
   has_many :means
   has_many :variances
 
+  def connected?
+    spark_core.info["connected"] == true
+  rescue
+    false
+  end
+
   def fetch_data
     means     << Mean.new(value: spark_core.variable("mean"))
     variances << Variance.new(value: spark_core.variable("variance"))
   end
+
+  # private
 
   def spark_core
     @spark_core ||= RubySpark::Core.new(spark_id)
---
+++ lib/tasks/fetch_data.rake
@@ -0,0 +1,8 @@
+task "data:collect" => :environment do
+  Core.all.each do |core|
+    if core.connected?
+      puts "Collecting data for #{core.name}"
+      core.fetch_data
+    end
+  end
+end

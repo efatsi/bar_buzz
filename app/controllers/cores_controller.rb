eda8c9c38a2f088606110e1798514bcea5afd093
Viget Spare <hardware-spare@viget.com>
2015-05-18 11:52:16 -0600

    Can filter results by day

--- app/controllers/cores_controller.rb
+++ app/controllers/cores_controller.rb
@@ -6,6 +6,20 @@ class CoresController < ApplicationController
 
   def show
     @core = Core.find(params[:id])
+    @days = @core.means.pluck(:day).uniq.sort
+
+    @means     = @core.means.where(day: day)
+    @variances = @core.variances.where(day: day)
+  end
+
+  private
+
+  def day
+    @day ||= if params[:day]
+      Date.parse(params[:day])
+    else
+      Date.today
+    end
   end
 
 end
